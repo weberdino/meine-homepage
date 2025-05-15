b8a1a0f609823090eb48294923acd410124690cc
dinovater <weber.dino@gmail.com>
2025-05-15 18:39:23 +0200

    esm rollback

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meine-homepage/', // z.B. '/meine-homepage/'
   plugins: [react()],
-  css:{
-    postcss: './postcss.config.js'
+  css: {
+    postcss: './postcss.config.cjs',
   },
-})
+});
