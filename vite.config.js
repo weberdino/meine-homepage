ea2118236130665ecac569047ce15aceef5961d8
dinovater <weber.dino@gmail.com>
2025-05-15 17:53:50 +0200

    changed from ESM to module, no success

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/meine-homepage/', // z.B. '/meine-homepage/'
   plugins: [react()],
+  css:{
+    postcss: './postcss.config.js'
+  },
 })
